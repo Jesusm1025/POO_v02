fb64815862a705b7c5d0e8fd10a160fb9bce81f3
Jesus Miguel Marcelino Kranwinkel <jesus.marcelinok@gmail.com>
2020-02-29 14:27:33 -0400

    POO_v02

---
+++ POO_v02/POO_v02/APARTAMENTO.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_v02
+{
+    class APARTAMENTO
+    {
+        //Crear una aplicación con los siguientes requerimientos.Crear una clase con 
+        //el nombre APARTAMENTO, con los atributos: Dirección, Color, área construcción, 
+        //Parqueos, Habitaciones y Baños.Declaramos los métodos VENDER y ALQUILAR, en 
+        //ellos mostraremos la información de la acción y las características.
+
+        //Agregando los atributos de la clase.
+        public string Direccion;
+        public string Color;
+        public string Area_Construccion;
+        public int Parqueo;
+        public int Habitaciones;
+        public int Baños;
+
+        public APARTAMENTO()//Ingresando los datos por consola.
+        {
+            Console.Write("Introduzca la dirreccion: ");
+            Direccion = Console.ReadLine();
+            Console.Write("Introduzca el color: ");
+            Color = Console.ReadLine();
+            Console.Write("Introduzca el area de construccion: ");
+            Area_Construccion = Console.ReadLine();
+            Console.Write("Introduzca la cantidad de parqueos: ");
+            Parqueo = int.Parse(Console.ReadLine());
+            Console.Write("Introduzca la cantidad de habitaciones: ");
+            Habitaciones = int.Parse(Console.ReadLine());
+            Console.Write("Introduzca la cantidad de baños: ");
+            Baños = int.Parse(Console.ReadLine()); 
+        }
+        public void vender()//Metodo vender.
+        {
+            Console.Clear();
+            Console.WriteLine("Venta de apartamento.");
+            Console.WriteLine("Dirrecion: " + Direccion);
+            Console.WriteLine("Color: " + Color);
+            Console.WriteLine("Area de construccion: " + Area_Construccion);
+            Console.WriteLine("Parqueos: " + Parqueo);
+            Console.WriteLine("Habitaciones: " + Habitaciones);
+            Console.WriteLine("Baños: " + Baños);
+            Console.ReadKey();
+            Console.Clear();
+            Console.Write("Volviendo al menu.");
+            Console.ReadKey();
+        }
+        public void alquilar()//Metodo alquilar.
+        {
+            Console.Clear();
+            Console.WriteLine("Alquiler de apartamento.");
+            Console.WriteLine("Dirrecion: " + Direccion);
+            Console.WriteLine("Color: " + Color);
+            Console.WriteLine("Area de construccion: " + Area_Construccion);
+            Console.WriteLine("Parqueos: " + Parqueo);
+            Console.WriteLine("Habitaciones: " + Habitaciones);
+            Console.WriteLine("Baños: " + Baños);
+            Console.ReadKey();
+            Console.Clear();
+            Console.Write("Volviendo al menu.");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ POO_v02/POO_v02/Program.cs
@@ -0,0 +1,55 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_v02
+{
+    class Program
+    {
+    //Crear una aplicación con los siguientes requerimientos.Crear una clase con 
+    //el nombre APARTAMENTO, con los atributos: Dirección, Color, área construcción, 
+    //Parqueos, Habitaciones y Baños. Declaramos los métodos VENDER y ALQUILAR, en 
+    //ellos mostraremos la información de la acción y las características.
+
+    //Los datos deben ser introducidos por el usuario y el objeto debe ser 
+    //instanciado desde la clase principal los métodos serán invocados por el 
+    //usuario de acuerdo a la acción que desee realizar.
+        static void Main(string[] args)
+        {
+            int opcion;//Variable para menu.
+            APARTAMENTO apartamento = new APARTAMENTO();//Declarar objeto.
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Vender.");
+                Console.WriteLine("2. Alquilar.");
+                Console.WriteLine("3. salir.");
+                Console.Write("Accediendo a la opcion: ");
+                opcion = int.Parse(Console.ReadLine());
+            
+                switch (opcion)
+                {
+                    case 1:
+                        apartamento.vender();//Accediendo al metodo vender de la clase apartamento. 
+                        break;
+                    case 2:
+                        apartamento.alquilar();//Accediendo al metodo alquilar de la clase apartamento.
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("Saliendo");
+                        Console.Read();
+                        break;
+                    default:
+                        Console.Write("Introduzca la opcion correcta.");
+                        break;
+                }
+            }
+            while (opcion != 3);//Mientras opcion sea diferente a 3, el menu se estara repitiendo.
+            Console.ReadKey();
+        }
+    }
+}
